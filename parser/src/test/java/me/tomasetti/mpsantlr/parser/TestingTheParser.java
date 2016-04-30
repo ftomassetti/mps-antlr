d21bb56d7f8afd4bb115ab467f6f93a8aa53fb37
Federico Tomassetti <federico@tomassetti.me>
2016-04-30 09:39:25 +0200

    initial version of the antlr parser facade

---
+++ parser/src/test/java/me/tomasetti/mpsantlr/parser/TestingTheParser.java
@@ -0,0 +1,10 @@
+package me.tomasetti.mpsantlr.parser;
+
+public class TestingTheParser {
+
+    @Test
+    public void parseEmptyLexer() {
+        String src = "lexer grammar LexBasic;";
+    }
+
+}
